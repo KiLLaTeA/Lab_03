c5057002aa7c96f921041a85320c8e4961495f2f
KiLLaTeA <poster173524@mail.ru>
2024-10-03 01:42:21 +0300

    Дизайн

--- index.js
+++ index.js
@@ -7,11 +7,28 @@ app.listen(port, ()=>{
 });
 
 app.use(express.static(`public`));
+app.use(express.urlencoded({extended: true}));
 
 let hbs = require(`hbs`);
 app.set(`views`, `views`);
 app.set(`view engine`, `hbs`);
 
+var paginate = require('handlebars-paginate');
+hbs.registerHelper('paginate', paginate);
+
 app.get(`/`, (req, res)=>{
-    res.render(`index`);
+    let page = "";
+    if (req.query.p == "" || req.query.p == undefined || req.query.p == NaN){
+        page = 1;
+    }
+    else{
+        page = req.query.p;
+    }
+
+    res.render(`index`, {
+        pagination: {
+            page: page,      // The current page the user is on
+            pageCount: 10    // The total number of available pages
+        }
+    });
 });
