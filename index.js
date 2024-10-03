d9eb46c2f7d5302b010d163d06492671b08adc61
KiLLaTeA <poster173524@mail.ru>
2024-10-03 22:54:03 +0300

    Готово

--- index.js
+++ index.js
@@ -14,54 +14,115 @@ app.set(`views`, `views`);
 app.set(`view engine`, `hbs`);
 
 var paginate = require('handlebars-paginate');
-const { faker } = require('@faker-js/faker');
+const { faker, id_ID } = require('@faker-js/faker');
 hbs.registerHelper('paginate', paginate);
 
-// const animals = ['ant', 'bison', 'camel', 'duck', 'elephant'];
-
-// console.log(animals.slice(2));
-// // Expected output: Array ["camel", "duck", "elephant"]
-
-// console.log(animals.slice(2, 4));
-// // Expected output: Array ["camel", "duck"]
-
-hbs.registerHelper('EqualSex', (v1, v2) => v1 == v2);
+hbs.registerHelper('EqualTerms', (v1, v2) => v1 == v2);
+hbs.registerHelper('EqualTermsNot', (v1, v2) => v1 != v2);
 
 dogs = [];
 for (let i = 0; i < 178; i++){
     dogs.push(
         {
+            _id: i,
             nick: faker.person.firstName(),
             sex: faker.person.sex(),
             breed: faker.animal.dog(),
-            age: faker.number.int({ min: 1, max: 16 }),
+            age: faker.number.int({ min: 1, max: 15 }),
             desc: faker.lorem.paragraph(1),
             img: faker.image.urlLoremFlickr({ width: 256, height: 256, category: 'dog' }),
+            isAdmin : false,
+            isHome : false,
         },
     );
 }
 
 app.get(`/`, (req, res)=>{
-    let page = "";
-    if (req.query.p == "" || req.query.p == undefined || req.query.p == NaN){
-        page = 1;
-    }
-    else{
-        page = req.query.p;
-    }
+    res.render(`index`, {
+        dogs: dogs.filter((item) => item.isHome == false),
+    });
+});
 
-    startEntry = page*16-16;
-    endEntry = page*16-1;
+app.post(`/`, (req, res)=>{
+    console.log(req.body.notHome);
+    console.log(dogs[req.body.notHome]["isAdmin"]);
+    dogs[req.body.notHome]["isAdmin"] = true;
+    console.log(dogs[req.body.notHome]["isAdmin"]);
+    res.render(`index`, {
+        dogs: dogs.filter((item) => item.isHome == false),
+    })
+})
 
-    if(endEntry > dogs[dogs.length - 1]){
-        endEntry = dogs[dogs.length - 1];
+
+app.get(`/sorting`, (req, res)=>{
+    let dogsSort = [];
+    let dogsNames = [];
+
+    if (req.query.sex == undefined && req.query.nick == ""){
+        dogsSort = dogs;
+    }
+    else if(req.query.sex == undefined && req.query.nick != ""){
+        dogsSort = dogs.filter((item) => item.nick == req.query.nick);
+    }
+    else if(req.query.sex != "" && req.query.nick == ""){
+        dogsSort = dogs.filter((item) => item.sex == req.query.sex);
+    }
+    else if(req.query.sex != "" && req.query.nick != ""){
+        dogsNames = dogs.filter((item) => item.sex == req.query.sex);
+        dogsSort = dogsNames.filter((item) => item.nick == req.query.nick);
     }
 
     res.render(`index`, {
-        pagination: {
-            page: page,      // The current page the user is on
-            pageCount: Math.ceil(dogs.length/16)    // The total number of available pages
-        },
-        dogs: dogs,
+        dogs: dogsSort,
     });
+})
+
+app.post(`/sorting`, (req, res)=>{
+    let dogsSort = [];
+    let dogsNames = [];
+
+    console.log(req.body.notHome);
+    console.log(dogs[req.body.notHome]["isAdmin"]);
+    dogs[req.body.notHome]["isAdmin"] = true;
+    console.log(dogs[req.body.notHome]["isAdmin"]);
+
+    if (req.query.sex == undefined && req.query.nick == ""){
+        dogsSort = dogs;
+    }
+    else if(req.query.sex == undefined && req.query.nick != ""){
+        dogsSort = dogs.filter((item) => item.nick == req.query.nick);
+    }
+    else if(req.query.sex != "" && req.query.nick == ""){
+        dogsSort = dogs.filter((item) => item.sex == req.query.sex);
+    }
+    else if(req.query.sex != "" && req.query.nick != ""){
+        dogsNames = dogs.filter((item) => item.sex == req.query.sex);
+        dogsSort = dogsNames.filter((item) => item.nick == req.query.nick);
+    }
+
+    res.render(`index`, {
+        dogs: dogsSort,
+    });
+})
+
+app.get(`/profile`, (req, res) => {
+    res.render(`profile`, {
+        dogs: dogs.filter((item) => item.isAdmin == true),
+    })
 });
+
+app.get(`/admin`, (req, res) => {
+    res.render(`admin`, {
+        dogs: dogs.filter((item) => item.isAdmin == true),
+    })
+});
+
+app.post(`/admin`, (req, res)=>{
+    console.log(req.body.sendDog);
+    console.log(dogs[req.body.sendDog]["isHome"]);
+    dogs[req.body.sendDog]["isHome"] = true;
+    console.log(dogs[req.body.sendDog]["isHome"]);
+    res.render(`admin`, {
+        dogs: dogs.filter((item) => item.isAdmin == true),
+    })
+})
